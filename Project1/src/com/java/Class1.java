971402725d7b871e81aeb14e546d8f7032edd080
user <user@DESKTOP-AHGNVC0>
2023-11-28 16:01:49 +0530

    updated

--- Project1/src/com/java/Class1.java
+++ Project1/src/com/java/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Demo of GIT config");
+System.out.println("Demo for GIT and GIT HUB config");
 	}
 
 }
